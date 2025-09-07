81b9b2fec90ea662a26167167633cceaa9daaa28
v.svir <vladsvir888@gmail.com>
2025-09-08 00:24:17 +0300

    refactor: env

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-// import { login, register } from '@/firebase/handlers';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { Alert, Button, Form, Input } from 'antd';
--- src/consts/firebase.ts
+++ src/consts/firebase.ts
@@ -1,2 +1,2 @@
 export const commonErrMessage = 'An error occurred while processing request.';
-export const tokenName = process.env.NEXT_PUBLIC_AUTH_COOKIE_NAME as string;
+export const tokenName = process.env.AUTH_COOKIE_NAME as string;
