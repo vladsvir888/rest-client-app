f5f0e5bb4b8db5d6514ce1549d6045207da69708
v.svir <vladsvir888@gmail.com>
2025-09-13 17:25:28 +0300

    fix: connection between variables and user

--- src/app/[locale]/variables/page.tsx
+++ src/app/[locale]/variables/page.tsx
@@ -1,23 +1,17 @@
-import { useTranslations } from 'next-intl';
 import VariableInfo from '@/components/VariableInfo';
 import AuthRoute from '@/components/auth/AuthRoute';
+import { checkAuth } from '@/app/actions/auth';
+import VariableTitle from '@/components/VariableTitle';
 
-export default function Variables() {
-  const t = useTranslations();
-
-  // if (!authUser) redirect('/login');
-  const user = 'user1';
+export default async function Variables() {
+  const { userEmail } = await checkAuth();
 
   return (
     <AuthRoute>
       <div className="container">
         <div className="private-layout">
-          <h1>{t('variables_page_title')}</h1>
-          {/* {typeof authUser === 'string' && (
-          <> */}
-          <VariableInfo authUser={user} />
-          {/* </>
-        )} */}
+          <VariableTitle />
+          <VariableInfo authUser={userEmail as string} />
         </div>
       </div>
     </AuthRoute>
