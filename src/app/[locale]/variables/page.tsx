c37affa671eabe7a36650ca1f22109baad243a34
v.svir <vladsvir888@gmail.com>
2025-09-13 16:43:35 +0300

    fix: variables

--- src/app/[locale]/variables/page.tsx
+++ src/app/[locale]/variables/page.tsx
@@ -1,23 +1,25 @@
-import { redirect } from 'next/navigation';
 import { useTranslations } from 'next-intl';
 import VariableInfo from '@/components/VariableInfo';
+import AuthRoute from '@/components/auth/AuthRoute';
 
-export default function Variables({ authUser }: { authUser: string | false }) {
+export default function Variables() {
   const t = useTranslations();
 
   // if (!authUser) redirect('/login');
   const user = 'user1';
 
   return (
-    <div className="container">
-      <div className="private-layout">
-        <h1>{t('variables_page_title')}</h1>
-        {/* {typeof authUser === 'string' && (
+    <AuthRoute>
+      <div className="container">
+        <div className="private-layout">
+          <h1>{t('variables_page_title')}</h1>
+          {/* {typeof authUser === 'string' && (
           <> */}
-        <VariableInfo authUser={user} />
-        {/* </>
+          <VariableInfo authUser={user} />
+          {/* </>
         )} */}
+        </div>
       </div>
-    </div>
+    </AuthRoute>
   );
 }
